f10217a8c0aa41dde34267d4893cf98443874d57
camillapalombi <camilla.palombi14@gmail.com>
2022-05-18 17:05:56 +0200

    Sistemazione rotte

--- laravel-auth/routes/web.php
+++ laravel-auth/routes/web.php
@@ -15,8 +15,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('homepage');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')->name('home');
+        Route::resource('/posts', 'PostController');
+    });
